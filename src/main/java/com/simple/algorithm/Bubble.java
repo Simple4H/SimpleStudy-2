85af627e323c37372618b477e532b9c2b44de93c
simple <crescentcxm@gmail.com>
2018-09-12 15:34:19 +0800

    冒泡排序

---
+++ src/main/java/com/simple/algorithm/Bubble.java
@@ -0,0 +1,29 @@
+package com.simple.algorithm;
+
+import java.util.Arrays;
+
+/**
+ * Create By S I M P L E On 2018/09/12 15:18:11
+ */
+public class Bubble {
+
+    public static void main(String[] args) {
+        int[] a = {3, 6, 1, 4, 5, 3, 6, 7, 212, 23, 41, 3};
+        bubbleSort(a);
+    }
+
+    private static void bubbleSort(int[] array){
+        //外层循环移动游标
+        for (int i = array.length - 1; i > 0; i--) {
+            //内层循环遍历游标及之后(或之前)的元素
+            for(int j = 0; j < i; j++){
+                if(array[j] > array[j+1]){
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                    System.out.println("Sorting: " + Arrays.toString(array));
+                }
+            }
+        }
+    }
+}
