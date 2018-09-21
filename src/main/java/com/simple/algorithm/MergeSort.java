bd29ba27d3c2290a7cd3c30c5b4b5efebe2fef60
simple <crescentcxm@gmail.com>
2018-09-21 19:57:59 +0800

    归并排序

--- src/main/java/com/simple/algorithm/MergeSort.java
+++ src/main/java/com/simple/algorithm/MergeSort.java
@@ -21,16 +21,16 @@ public class MergeSort {
         if (start >= end)
             return;
         int len = end - start, mid = (len >> 1) + start;
-        int start1 = start, end1 = mid;
-        int start2 = mid + 1, end2 = end;
-        merge_sort_recursive(arr, result, start1, end1);
-        merge_sort_recursive(arr, result, start2, end2);
+        int start1 = start;
+        int start2 = mid + 1;
+        merge_sort_recursive(arr, result, start1, mid);
+        merge_sort_recursive(arr, result, start2, end);
         int k = start;
-        while (start1 <= end1 && start2 <= end2)
+        while (start1 <= mid && start2 <= end)
             result[k++] = arr[start1] < arr[start2] ? arr[start1++] : arr[start2++];
-        while (start1 <= end1)
+        while (start1 <= mid)
             result[k++] = arr[start1++];
-        while (start2 <= end2)
+        while (start2 <= end)
             result[k++] = arr[start2++];
         for (k = start; k <= end; k++)
             arr[k] = result[k];
@@ -43,6 +43,7 @@ public class MergeSort {
     }
 
     // 迭代版
+    @SuppressWarnings("ConstantConditions")
     private static void mergeSort2(int[] arr) {
         int[] orderedArr = new int[arr.length];
         for (int i = 2; i < arr.length * 2; i *= 2) {
