1a87e1f5071a9e9b16a7a095cbeda32a1e31702a
simple <crescentcxm@gmail.com>
2018-09-25 17:48:15 +0800

    交换值

--- src/main/java/com/simple/algorithm/Swap.java
+++ src/main/java/com/simple/algorithm/Swap.java
@@ -2,6 +2,8 @@ package com.simple.algorithm;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.lang.reflect.Field;
+
 /**
  * Create By S I M P L E On 2018/09/25 17:32:18
  */
@@ -13,14 +15,23 @@ public class Swap {
         int a = 1;
         int b = 2;
         log.warn("a:{},b:{}", a, b);
-        swap(a, b);
+        swap2(a, b);
         log.warn("a:{},b:{}", a, b);
     }
 
     // 错误的示范
-    private static void swap(Integer a, Integer b) {
+    private static void swap1(Integer a, Integer b) {
         int temp = a;
         a = b;
         b = temp;
+    }
+
+    // 正确的示范
+    private static void swap2(Integer a, Integer b) throws NoSuchFieldException, IllegalAccessException {
+        int temp = a;
+        Field value = Integer.class.getDeclaredField("value");
+        value.setAccessible(true);
+        value.set(a, b);
+        value.set(b, new Integer(temp));
     }
 }
