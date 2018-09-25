c103b1da842983ed01f34e74e0c2d372dfdcc9d7
simple <crescentcxm@gmail.com>
2018-09-25 17:49:56 +0800

    reason

--- src/main/java/com/simple/algorithm/Swap.java
+++ src/main/java/com/simple/algorithm/Swap.java
@@ -32,6 +32,10 @@ public class Swap {
         Field value = Integer.class.getDeclaredField("value");
         value.setAccessible(true);
         value.set(a, b);
+        // value.set的时候将Integer的缓存值改变了，
+        // 因为value.set(Object v1, Object v2)两个参数都是对象类型，
+        // 所以temp会进行自动装箱操作，会调用valueOf方法，这样会获取到错误的缓存值，
+        // 所以，为了避免这种情况，就只能不需要调用缓存值，直接new Integer就可以跳过缓存。
         value.set(b, new Integer(temp));
     }
 }
