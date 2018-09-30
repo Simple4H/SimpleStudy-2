dec89fd21317d151d59cf67dd28317dbbda0e256
simple <crescentcxm@gmail.com>
2018-09-30 11:26:59 +0800

    使用Redis Template

--- src/test/java/com/simple/SimpleStudy2ApplicationTests.java
+++ src/test/java/com/simple/SimpleStudy2ApplicationTests.java
@@ -1,15 +1,22 @@
 package com.simple;
 
+import com.simple.redis.User;
 import lombok.extern.slf4j.Slf4j;
+import org.checkerframework.checker.units.qual.A;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Slf4j
 public class SimpleStudy2ApplicationTests {
+
+    @Autowired
+    private RedisTemplate<String,Object> redisTemplate;
 
     @Test
     public void contextLoads() {
@@ -20,4 +27,16 @@ public class SimpleStudy2ApplicationTests {
         }
     }
 
+    @Test
+    public void redisTest() {
+        User user = new User("simple","password");
+        Boolean flag;
+        flag = redisTemplate.opsForValue().setIfAbsent("user",user);
+        if (flag){
+            log.warn("User:{}",redisTemplate.opsForValue().get("user"));
+        }
+        else {
+            log.error("the key is exist");
+        }
+    }
 }
